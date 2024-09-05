201cf3061aac932e3ff1868661995c621ae091df
DishaGabale <141235126+DishaGabale@users.noreply.github.com>
2024-09-05 11:57:05 +0530

    Update sidebar.py

--- src/sidebar.py
+++ src/sidebar.py
@@ -11,7 +11,7 @@ contactInfo = """
 ### You could connect with me on [LinkedIn](https://www.linkedin.com/in/disha-gabale-9682ba238/) or could mail me [here](mailto:gabaledisha@gmail.com)
 """
 
-# examples = [example1, example2, example3]
+
 
 def sidebar_conf():
     sidebar = st.sidebar
@@ -21,12 +21,7 @@ def sidebar_conf():
     sidebar.markdown(summary)
     sidebar.caption(preinfo)
     sidebar.markdown("* To simply generate a article")
-    # sidebar.info(examples[0])
     sidebar.markdown("* To generate articles about certain latest government policy updates ")
-    # sidebar.info(examples[1])
-    # sidebar.markdown("* If you wanna keep it to certain words")
-    # sidebar.info(examples[2])
-    # sidebar.caption(afterinfo)
     sidebar.divider()
     sidebar.markdown(contactInfo)
     sidebar.markdown("# Made with ❤️ by Disha")
